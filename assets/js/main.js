840a43893e2435c681c550b922a444a43ee29324
anaghaaa.r <r.anaghaaa@gmail.com>
2026-05-15 02:00:54 +0530

    fixed website responsive issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 function toggleFaq(el) {
     el.classList.toggle('open');
 }
+
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("show");
+    document.querySelector(".nav-right").classList.toggle("show");
+    document.querySelector(".menu-toggle").classList.toggle("open");
+}
